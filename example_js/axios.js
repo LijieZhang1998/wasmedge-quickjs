aeaa6610016af0f857410a9aacb78bb5ddb138d4
LijieZhang1998 <lijie.zhang@mongodb.com>
2024-05-15 15:24:26 -0500

    Add exception

--- example_js/axios.js
+++ example_js/axios.js
@@ -3,6 +3,11 @@ import redaxios from "redaxios";
 
 const axios = redaxios({ fetch, responseType: "json" });
 
-axios.get("http://localhost:8000").then((response) => {
-  console.log("connected");
-});
+axios
+  .get("http://localhost:8000")
+  .then((response) => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log("exception: ", e);
+  });
